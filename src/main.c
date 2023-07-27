a2946809c70ca586061be8f11d9c55f4eec38818
Ramo <ramo160689@gmail.com>
2023-07-27 15:47:06 +0200

    update function now projects triangles structs and render function draw triangles

--- src/main.c
+++ src/main.c
@@ -10,10 +10,9 @@
 #include "memory.h"
 #include "display.h"
 #include "vector.h"
+#include "mesh.h"
 
-#define N_POINTS (9*9*9)
-vec3_t cube_points[N_POINTS];
-vec2_t projected_points[N_POINTS];
+triangle_t triangles_to_render[N_MESH_FACES];
 
 vec3_t camera_position = { .x = 0.f, .y = 0.f, .z = -5.f };
 vec3_t cube_rotation = { .x = 0.f, .y = 0.f, .z = 0.f };
@@ -65,17 +64,36 @@ void gameUpdate(void)
     cube_rotation.z += 0.02f;
     cube_rotation.x += 0.02f;
 
-    for (int i = 0; i < N_POINTS; ++i)
+    for (int i = 0; i < N_MESH_FACES; i++)
     {
-        vec3_t point = cube_points[i];
-        
-        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
-        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
-        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+        face_t mesh_face = mesh_faces[i];
+        vec3_t face_vertices[3];
+        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
 
-        // Translate the points away from the camera
-        transformed_point.z -= camera_position.z;
-        projected_points[i] = project(transformed_point);
+        triangle_t projected_triangle = {};
+
+        for (int j = 0; j < 3; j++)
+        {
+            vec3_t transformed_vertex = face_vertices[j];
+            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+
+            // Translate the vertex away from the camera
+            transformed_vertex.z -= camera_position.z;
+            
+            vec2_t projected_vertex = project(transformed_vertex);
+
+            // Scale and translate the projected points to the middle of the screen
+            projected_vertex.x += playDate->display->getWidth() / 2;
+            projected_vertex.y += playDate->display->getHeight() / 2;
+
+            projected_triangle.points[j] = projected_vertex;
+        }
+
+        triangles_to_render[i] = projected_triangle;
     }
 }
 
@@ -83,10 +101,20 @@ void render(void)
 {
     clearFrameBuffer(kColorBlack);
     drawGrid(kColorWhite);
-    for (int i = 0; i < N_POINTS; ++i)
+	
+    for (int i = 0; i < N_MESH_FACES; i++)
     {
-        drawRect(projected_points[i].x + (playDate->display->getWidth() / 2), projected_points[i].y + (playDate->display->getHeight() / 2), 4, 4, kColorWhite);
+        //for (int j = 0; j < 3; j++)
+        //{
+        //    drawRect(triangles_to_render[i].points[j].x, triangles_to_render[i].points[j].y, 3, 3, kColorWhite);
+        //}
+
+        drawTriangle(triangles_to_render[i].points[0].x, triangles_to_render[i].points[0].y,
+                     triangles_to_render[i].points[1].x, triangles_to_render[i].points[1].y,
+                     triangles_to_render[i].points[2].x, triangles_to_render[i].points[2].y,
+                     kColorWhite);
     }
+	
     renderColorBuffer();
 }
 
