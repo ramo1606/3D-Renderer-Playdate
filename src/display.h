919926fe569ef6a823e2187917aaf688d01940c2
Ramo <ramo160689@gmail.com>
2023-07-27 15:42:19 +0200

    Declare drawLine and drawTriangle functions

--- src/display.h
+++ src/display.h
@@ -6,6 +6,8 @@ void initDisplay(PlaydateAPI* pd);
 
 /* Drawing Functions */
 void drawPixel(int x, int y, LCDSolidColor color);
+void drawLine (int x0, int y0, int x1, int y1, LCDSolidColor color);
+void drawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, LCDSolidColor color);
 void drawRect(int x, int y, int width, int height, LCDSolidColor color);
 void drawGrid(LCDSolidColor color);
 
